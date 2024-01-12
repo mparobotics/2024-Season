57d30cd2341958d06ed5033b7df95bcb708146a6
leakin24mpa <114780668+leakin24mpa@users.noreply.github.com>
2024-01-11 19:23:34 -0600

    prototyping project

---
+++ 2024 Prototyping/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.subsystems.MotorSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+
+
+
+
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  MotorSubsystem m_MotorSubsystem = new MotorSubsystem();
+
+
+  private final CommandXboxController xbox =
+      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    configureBindings();
+  }
+
+
+  private void configureBindings() {
+    //control the motor with the left joystick
+    m_MotorSubsystem.setDefaultCommand(m_MotorSubsystem.setPositionPID(() -> xbox.getLeftY()));
+  }
+
+
+  public Command getAutonomousCommand() {
+    //no auto (yet...)
+    return null;
+  }
+}
