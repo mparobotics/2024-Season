a544285c3b70e600a6cb5dbbc41a00ceab59316c
leakin24mpa <114780668+leakin24mpa@users.noreply.github.com>
2024-02-08 16:29:20 -0600

    Trapezoidal
    Implemented WPIlib's trapezoidal motion profiling

--- 2024 Full Robot Code/src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ 2024 Full Robot Code/src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -4,70 +4,42 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.SensorCollection;
-import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
-import com.ctre.phoenix6.configs.MotionMagicConfigs;
-import com.ctre.phoenix6.configs.Slot0Configs;
-import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
+
 import com.ctre.phoenix6.controls.Follower;
-import com.ctre.phoenix6.controls.MotionMagicVoltage;
-import com.ctre.phoenix6.controls.PositionVoltage;
+
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.GravityTypeValue;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.TrapezoidProfileSubsystem;
 import frc.robot.Constants.ArmConstants;
 
-public class ArmSubsystem extends SubsystemBase {
+public class ArmSubsystem extends TrapezoidProfileSubsystem {
   //two motors run in sync to drive the arm
   private final TalonFX motorR = new TalonFX(ArmConstants.RmotorID);
   private final TalonFX motorL = new TalonFX(ArmConstants.LmotorID);
   
   private final RelativeEncoder armEncoder = new CANSparkMax(ArmConstants.encoderID, MotorType.kBrushed).getEncoder();
   //configurations for the motors and control loop
-  private MotionMagicConfigs MMconfig = new MotionMagicConfigs();
-  private SoftwareLimitSwitchConfigs limitConfig = new SoftwareLimitSwitchConfigs();
-  private Slot0Configs PIDconfig = new Slot0Configs();
+  private PIDController armPID = new PIDController(ArmConstants.kP, ArmConstants.kI, ArmConstants.kD);
+  private ArmFeedforward armFF= new ArmFeedforward(ArmConstants.kS, ArmConstants.kG, ArmConstants.kV, ArmConstants.kA);
 
-  private MotionMagicVoltage MMcontroller = new MotionMagicVoltage(0);
   private double setpoint;
   /** Creates a new ArmSubsystem. */
   public ArmSubsystem() {
-    limitConfig.ForwardSoftLimitEnable = true;
-    limitConfig.ReverseSoftLimitEnable = true;
+    
+    
+    super(new TrapezoidProfile.Constraints(ArmConstants.kMaxVelocity,ArmConstants.kMaxAcceleration));
 
-    limitConfig.ReverseSoftLimitThreshold = ArmConstants.maxArmPosition;
-    limitConfig.ReverseSoftLimitThreshold = ArmConstants.minArmPosition;
     
-    MMconfig.MotionMagicAcceleration = ArmConstants.kMaxAcceleration;
-    MMconfig.MotionMagicCruiseVelocity = ArmConstants.kMaxVelocity;
-    
-    
-    /*gravity affects the arm's motion differently depending on the arm's position.
-    the force of gravity has the greatest effect when the arm is horizontal and zero effect when the arm is vertical
-    this can be represented as the cosine of the arm's angle.*/
-    PIDconfig.GravityType = GravityTypeValue.Arm_Cosine;
-
-    /*The Arm is controlled using a combination of Feedforward control and PID control. 
-    The feedforward estimates the voltage that should be supplied to the motors to get them 
-    to move at a desired velocity and acceleration, however it isn't perfect, so we use a PID loop to correct for any error that builds up */
-    
-    //configure feedforward
-    PIDconfig.kG = ArmConstants.kG;
-    PIDconfig.kS = ArmConstants.kS;
-    PIDconfig.kV = ArmConstants.kV;
-    PIDconfig.kA = ArmConstants.kA;
-    //configure PID
-    PIDconfig.kP = ArmConstants.kP;
-    PIDconfig.kI = ArmConstants.kI;
-    PIDconfig.kD = ArmConstants.kD;
-
-    motorR.getConfigurator().apply(PIDconfig);
-    motorR.getConfigurator().apply(MMconfig);
 
     //The left motor follows the right motor. The right motor runs the control loop, and the left motor copies the output of the right motor
     //opposite motor directions spin the arm in the same direction, so opposeMasterDirection is set to true
@@ -77,8 +49,9 @@ public class ArmSubsystem extends SubsystemBase {
   //move the arm to the desired position
   public void setTarget(double degrees){
     //use the contoller defined in slot0
-    motorR.setControl(MMcontroller.withPosition(Units.degreesToRotations(degrees)).withSlot(0));
-    setpoint = degrees;
+    setpoint = Units.degreesToRadians(degrees);
+    setGoal(setpoint);
+    
   }
   public double getArmPosition(){
     return 0;
@@ -87,10 +60,15 @@ public class ArmSubsystem extends SubsystemBase {
     return Math.abs(setpoint - getArmPosition()) < 0.01;
   }
   
-  
+  @Override
+  protected void useState(State state) {
+    motorR.setVoltage(armFF.calculate(state.position,state.velocity) + armPID.calculate(getArmPosition(),state.position));
+  }
   @Override
   public void periodic() {
     
    
   }
+
+  
 }
