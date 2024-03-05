a9b0ec5d2a42f08a8e76da87c85fb42e85d4a4bc
leakin24mpa <114780668+leakin24mpa@users.noreply.github.com>
2024-03-05 10:40:22 -0600

    button box code

---
+++ buttonBox/buttonBox.ino
@@ -0,0 +1,29 @@
+#include <Joystick.h>
+Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_GAMEPAD<
+1,0,false,false,false,false,false,false,false,false,false,false,false);
+void setup() {
+  pinMode(4, INPUT_PULLUP);
+  pinMode(5, INPUT_PULLUP);
+  pinMode(6, INPUT_PULLUP);
+  pinMode(7, INPUT_PULLUP);
+  pinMode(8, INPUT_PULLUP);
+  pinMode(9, INPUT_PULLUP);
+  pinMode(10, INPUT_PULLUP);
+  pinMode(11, INPUT_PULLUP);
+  pinMode(12, INPUT_PULLUP);
+  pinMode(13, INPUT_PULLUP);
+
+  Joystick.begin();
+}
+int lastButtonState[10] = {0,0,0,0,0,0,0,0,0,0};
+const int pinToButtonMap = 4;
+void loop() {
+  for(int i = 0; i < 10; i++){
+    int currentButtonState = !digitalRead(i + pinToButtonMap);
+    if(currentButtonState != lastButtonState[i]){
+      Joystick.setButton(i, currentButtonState);
+      lastButtonState[i] = currentButtonState;
+    }
+  }
+  delay(50);
+}
