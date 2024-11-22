d0fb82fc80046703ca7cb637aea3ab49ce3901b9
raininglilly <lliao26@moundsparkacademy.org>
2024-11-21 21:59:53 -0600

    .repeatedly added to controllers

--- tennisBallShooter/src/main/java/frc/robot/RobotContainer.java
+++ tennisBallShooter/src/main/java/frc/robot/RobotContainer.java
@@ -21,12 +21,13 @@ public class RobotContainer {
   }
 
   private void configureBindings() { //void: don't have set value, not returning any value.
-    controller.axisGreaterThan(Axis.kRightY.value,0.5).whileTrue(m_MotorSubsystem.RunMotors());
+    controller.axisGreaterThan(Axis.kRightY.value,0.5).whileTrue(m_MotorSubsystem.RunMotors().repeatedly());
     //y-axis, pass 0.5 the motor will spin up.
-    controller.axisLessThan(Axis.kRightY.value,-0.5).whileTrue(m_MotorSubsystem.RunMotors());
+    controller.axisLessThan(Axis.kRightY.value,-0.5).whileTrue(m_MotorSubsystem.RunMotors().repeatedly());
     //y-axis, pass -0.5 the motor will spin up the other way.
     m_MotorSubsystem.setDefaultCommand(m_MotorSubsystem.StopMotors());
     //Defalut for the motor, to stop the motor.
+    //.repeatedly() makes command keep running repeatedly as long as joystick is pressed down
   }
 
   public Command getAutonomousCommand() {
